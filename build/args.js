1419be97486d041a55c5400f246bb1c391f7c342
Moises Gonzalez <moises.gonzalez@flixbus.com>
2018-08-05 20:28:08 +0200

    Bumps version for inquirer parrotify

--- build/args.js
+++ build/args.js
@@ -2,7 +2,7 @@ const program = require('commander');
 
 class Args {
   constructor() {
-    program.version('1.0.7').option('-w --wizard', 'Use the wizrd to create your parrot!').option('-b --base [base]', 'Base parrot to use', /^(boredparrot|congaparrot|mega|middleparrot|parrot|rightparrot)$/i, 'parrot').option('-o --overlay [overlay]', 'Image to place on top of the parrot', '').option('-p --position [position]', 'Position of the overlay (face, hat or hand)', /^(face|hat|hand)$/i, 'face').option('-d --delay [delay]', 'How hard does the parrot party?', '').parse(process.argv);
+    program.version('1.0.8').option('-w --wizard', 'Use the wizrd to create your parrot!').option('-b --base [base]', 'Base parrot to use', /^(boredparrot|congaparrot|mega|middleparrot|parrot|rightparrot)$/i, 'parrot').option('-o --overlay [overlay]', 'Image to place on top of the parrot', '').option('-p --position [position]', 'Position of the overlay (face, hat or hand)', /^(face|hat|hand)$/i, 'face').option('-d --delay [delay]', 'How hard does the parrot party?', '').parse(process.argv);
     this.program = program;
   }
 }
